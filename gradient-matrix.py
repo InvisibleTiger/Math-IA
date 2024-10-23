4305626a64eaa629678cbf2d143ff5f1c1429bd4
Jeevan Adhya Vinoth Babu <jeevanadhya.vinothbabu@gmail.com>
2024-10-22 21:51:15 -0500

    playing around with the matrices

---
+++ gradient-matrix.py
@@ -0,0 +1,24 @@
+import numpy as np
+import cv2
+
+image_path = 'simple-pattern.png'
+image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+
+sobel_x = np.array([[-1, 0, 1],
+                    [-2, 0, 2],
+                    [-1, 0, 1]])
+
+sobel_y = np.array([[1, 2, 1],
+                    [0, 0, 0],
+                    [-1, -2, -1]])
+
+gradient_x = cv2.filter2D(image, cv2.CV_64F, sobel_x)
+gradient_y = cv2.filter2D(image, cv2.CV_64F, sobel_y)
+
+gradient_magnitude = np.sqrt(gradient_x**2 + gradient_y**2)
+
+gradient_magnitude = np.uint8(gradient_magnitude)
+
+np.savetxt('gradient_matrix.txt', gradient_magnitude, fmt='%d')
+
+print("Gradient matrix saved to 'gradient_matrix.txt'")
---
+++ image-matrix.py
@@ -0,0 +1,11 @@
+import numpy as np
+import cv2
+
+image_path = 'simple-pattern.png'
+image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+
+image_matrix = np.array(image)
+
+np.savetxt('image_matrix.txt', image_matrix, fmt='%d')
+
+print("Image matrix saved to 'image_matrix.txt'")
